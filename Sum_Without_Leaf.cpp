95532f457456e4b682221d6e361e5cce633a9a56
Md Rifat <mdrifatahmed787@gmail.com>
2024-08-29 20:14:52 +0600

    sum without leaf node

---
+++ Sum_Without_Leaf.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+     int val;
+     Node *left;
+     Node *right;
+
+     Node(int val){
+       this->val=val;
+       this->left=NULL;
+       this->right=NULL;
+    }
+};
+
+Node* input_tree(){
+    int val;
+    cin >> val;
+    Node *root;
+
+    if(val==-1) root=NULL;
+    else root=new Node(val);
+
+    queue<Node*>q;
+    if(root) q.push(root);
+    while(!q.empty()){
+        Node *p=q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+
+        Node *left;
+        Node *right;
+        if(l==-1) left=NULL;
+        else left=new Node(l);
+
+        if(r==-1) right=NULL;
+        else right=new Node(r);
+
+        p->left=left;
+        p->right=right;
+
+        if(p->left)q.push(p->left);
+        if(p->right)q.push(p->right);
+    }
+
+    return root;
+}
+
+void sum_without_leaf(Node *root){
+    if(root==NULL){
+        return;
+    };
+
+    queue<Node*>q;
+    q.push(root);
+    int sum=0;
+    while(!q.empty()){
+        Node *f=q.front();
+        q.pop();
+
+       if (f->left != NULL || f->right != NULL) {
+            sum += f->val;
+        }
+
+        if(f->left)q.push(f->left);
+        if(f->right)q.push(f->right);
+    }
+    cout<<sum<<endl;
+}
+ 
+int main(){
+    Node *root=input_tree();
+    sum_without_leaf(root);
+    return 0;
+}
